1e16a103a986be065aa9b9173cf03ca1d0596e23
shicks <shicks255@yahoo.com>
2019-04-22 23:27:30 -0400

    sorting columns

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -7,28 +7,31 @@ export default class Courses extends React.Component{
         super(props);
         this.state = {
             groupBy: 'year',
-            sortBy: 'grade'
+            sortBy: 'grade',
+            courses: this.props.courses
         }
         this.groups = ['year', 'school', 'season', ];
-        this.sorts = ['grade', 'alpha']
+        this.sortCols = ['d', 'd', 'd', 'd', 'd', 'd', 'd'];
         this.groupingByYear();
+
+        this.sortBy = this.sortBy.bind(this);
     }
 
     getSeasons()
     {
-        return new Set(this.props.courses.map(v => v.season));
+        return new Set(this.state.courses.map(v => v.season));
     }
 
     getYears()
     {
-        return new Set(this.props.courses.map(v => v.year));
+        return new Set(this.state.courses.map(v => v.year));
     }
 
     groupingByYear()
     {
         let map = {};
 
-        let courses = this.props.courses.sort((a,b) => {
+        let courses = this.state.courses.sort((a,b) => {
             if (a.year > b.year)
                 return 1;
             if (a.year < b.year)
@@ -45,25 +48,50 @@ export default class Courses extends React.Component{
         console.log(map);
     }
 
+    sortBy(field, col)
+    {
+        let sortedCourses = this.state.courses.sort((x,y) => {
+            return x[field] > y[field];
+        });
+        this.sortCols[col] = 'd';
+        if (this.sortCols[col] === 'd')
+        {
+            sortedCourses.reverse();
+            this.sortCols[col] = 'u';
+        }
+        this.setState({courses: sortedCourses});
+    }
+
     render()
     {
-
-
-        let courses = this.props.courses.map((v,i) => {
+        let courses = this.state.courses.map((v,i) => {
             return <tr key={v.id}>
                 <td>{v.year}</td>
                 <td>{v.season}</td>
                 <td>{v.school.acronim}</td>
                 <td>{v.courseCode}</td>
-                <td>{v.name}</td>
+                <td>{v.courseName}</td>
                 <td>{v.schoolYear}</td>
                 <td>{v.grade}</td>
+                <td></td>
             </tr>
         });
 
         return (
             <div>
                 <table>
+                    <thead>
+                    <tr>
+                        <th onClick={() => this.sortBy('year', 0)}>Year</th>
+                        <th onClick={() => this.sortBy('season', 1)}>Season</th>
+                        <th onClick={() => this.sortBy('school', 2)}>School</th>
+                        <th onClick={() => this.sortBy('courseCode', 3)}>Code</th>
+                        <th onClick={() => this.sortBy('courseName', 4)}>Name</th>
+                        <th onClick={() => this.sortBy('schoolYear', 5)}>School Year</th>
+                        <th onClick={() => this.sortBy('grade', 6)}>Grade</th>
+                        <th>Coursework</th>
+                    </tr>
+                    </thead>
                     <tbody>
                     {courses}
                     </tbody>
