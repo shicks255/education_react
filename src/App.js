9684f217a44bb2ec050790792832bb5bc326b2ad
shicks <shicks255@yahoo.com>
2019-04-22 21:06:44 -0400

    making objects for the courses

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Header from './components/Header';
-// import './App.css';
-import 'bulma/css/bulma.css';
 import Courses from "./components/Courses";
+import Header from './components/Header';
+import 'bulma/css/bulma.css';
+import Course from './models/Course';
 
 class App extends Component {
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            courseUrl: 'http://localhost:8080/course',
+            loaded: false
+        };
+    }
+
+
+    componentWillMount()
+    {
+        // console.log(this.state.courseUrl);
+        fetch(this.state.courseUrl)
+            .then(res => res.json())
+            .then(res => {
+                const courses = res.map(x =>{
+                    return new Course(x);
+                });
+                this.setState({courses: courses, loaded: true});
+            });
+    }
+
     render() {
+        let courses = this.state.loaded ? <Courses courses={this.state.courses}/> : '';
+
         return (
             <div className="App container">
                 <header className="App-header">
                     <Header></Header>
                 </header>
-                <Courses></Courses>
+                {courses}
             </div>
         );
     }
---
+++ src/models/Course.js
@@ -0,0 +1,17 @@
+
+export default class Course {
+
+    constructor(object){
+        this.id = object.id;
+        this.school = object.school;
+        this.courseCode = object.courseCode;
+        this.courseName = object.courseName;
+        this.grade = object.grade;
+        this.year = object.year;
+        this.schoolYear = object.schoolYear;
+        this.season = object.season;
+        this.coursework = object.coursework;
+    }
+
+
+}
